e73696e9cd862f3db8a9ac8d01f835ae434ca35a
Daniel Lop-Chi Cheng <dlcheng@bayonet-00.ics.uci.edu>
2017-01-19 22:41:11 -0800

    CHG: Resubmmitting s2g.sh documentation and error handling modifications

--- s2g.sh
+++ s2g.sh
@@ -1,4 +1,31 @@
+#Description:
+# SpArcFiRe to Galfit
+#Usage:
+# - Run on ics.uci.edu servers to access sparcfire
+# - Place png/fits files in 
+echo "Running SpArcFiRe"
 /home/sparcfire/bin/SpArcFiRe sparcfire_input/ sparcfire_temp/ sparcfire_output/
-galfit $1
+if [ "$?" != "0" ]; then
+	echo "SpArcFiRe error, aborting" 1>&2
+	exit 1
+fi
 
+#Run python conversion script on each sparfire output
+echo "Running python sparcfire to galfit conversion script"
+python main.py sparcfire_output/galaxy.csv sparcfire_output galaxy_arcs.csv
+if [ "$?" != "0" ]; then
+	echo "Python error, aborting" 1>&2
+	exit 1
+fi
+
+#Run galfit on each feedme file
+echo "Running GALFIT"
+#galfit galfit_input/*.feedme
+if [ "$?" != "0" ]; then
+	echo "GALFIT error, aborting" 1>&2
+	exit 1
+fi
+
+#Show original image, sparcfire output with arcs, galfit light curve, and subtraction
+echo "Showing images"
 
