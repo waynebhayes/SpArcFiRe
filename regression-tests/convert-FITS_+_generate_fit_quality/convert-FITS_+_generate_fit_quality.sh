df80d55b3a0b18981d9d26a897391094ebdf4d7b
John English <jeenglis@uci.edu>
2020-04-22 12:15:21 -0700

    I removed .swp files.  I also tried to clear out all of the unneeded things for testing.

--- regression-tests/convert-FITS_+_generate_fit_quality/convert-FITS_+_generate_fit_quality.sh
+++ regression-tests/convert-FITS_+_generate_fit_quality/convert-FITS_+_generate_fit_quality.sh
@@ -5,8 +5,8 @@ die() { echo "FATAL ERROR IN CONVERT FITS: $@" >&2; return 1;}
 source $SPARCFIRE_HOME/scripts/SpArcFiRe -convert-FITS $SPARCFIRE_HOME/regression-tests/convert-FITS_+_generate_fit_quality/test_data/G.in/ $SPARCFIRE_HOME/regression-tests/convert-FITS_+_generate_fit_quality/test_data/G.tmp $SPARCFIRE_HOME/regression-tests/convert-FITS_+_generate_fit_quality/test_data/G.out -generateFitQuality 1;
 
 #Cleanup log files
-rm *txt*;
-rm *galfit*;
+rm ./*settings.txt;
+
 if cmp -s "test_data/test_out/galaxy.csv" "test_data/G.out/galaxy.csv"; then
 	printf 'Pass'
 else
@@ -14,5 +14,5 @@ else
 fi;
 
 #Cleanup data created by test
-rm -rf test_data/G.out/*;
-rm -rf test_data/G.tmp/*;
+rm -rf $SPARCFIRE_HOME/regression-tests/convert-FITS_+_generate_fit_quality/test_data/G.out/*;
+rm -rf $SPARCFIRE_HOME/regression-tests/convert-FITS_+_generate_fit_quality/test_data/G.tmp/*;
--- regression-tests/convert-FITS_no_arguments/convert-FITS_no_argument.sh
+++ regression-tests/convert-FITS_no_arguments/convert-FITS_no_argument.sh
@@ -5,7 +5,7 @@ die() { echo "FATAL ERROR IN CONVERT FITS: $@" >&2; return 1;}
 source $SPARCFIRE_HOME/scripts/SpArcFiRe -convert-FITS $SPARCFIRE_HOME/regression-tests/convert-FITS_no_arguments/test_data/G.in/ $SPARCFIRE_HOME/regression-tests/convert-FITS_no_arguments/test_data/G.tmp $SPARCFIRE_HOME/regression-tests/convert-FITS_no_arguments/test_data/G.out;
 
 #Cleanup log files
-rm *.txt;
+rm ./*settings.txt;
 
 if cmp -s "test_data/test_out/galaxy.csv" "test_data/G.out/galaxy.csv"; then
 	printf 'Pass'
@@ -14,5 +14,5 @@ else
 fi;
 
 #Cleanup data created by test
-rm -rf test_data/G.out/*;
-rm -rf test_data/G.tmp/*;
+rm -rf  $SPARCFIRE_HOME/regression-tests/convert-FITS_no_arguments/test_data/G.out/*;
+rm -rf  $SPARCFIRE_HOME/regression-tests/convert-FITS_no_arguments/test_data/G.tmp/*;
