ffa3be8ba363c5c4158bf5ad531e1bfae5feae61
John English <jeenglis@uci.edu>
2020-04-28 10:14:24 -0700

    Updated testing script and setup.sh so it correctly handels python environments where the default python is python2.7 instead of having python2.7 explicitly without loosing old functionality.
    The goal was to get regressions tests working on jenkins.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 die() { echo "FATAL ERROR: $@" >&2; return 1
 }
 USAGE="USAGE:
@@ -16,20 +16,33 @@ to figure out how to set the environment variable SPARCFIRE_HOME yourself.
 
 echo "$USAGE"
 
-echo -n "Checking you have Python 2.7 installed: "
-python2.7 --version || die "You need to install Python 2.7, and have the executable called python2.7"
-PIP_NEED='numpy|Pillow|scipy|astropy'
-PIP_HAVE=`python2.7 -m pip list | egrep "$PIP_NEED"`
-if [ `echo "$PIP_HAVE" | wc -l` -ne 4 ]; then
-    echo "We need all of the following Python packages: `echo "$PIP_NEED" | sed 's/|/ /g'`"
-    echo But you only have the following:
-    echo "$PIP_HAVE"
-    echo "Please use 'python2.7 -m pip install <package>' to install any missing packages"
-    die "missing pip packages"
+echo -n "Checking you have Python 2.7 installed"
+PY_VERSION=`python --version`;
+if [[ $PY_VERSION == "2.7"* ]]; then
+    PIP_NEED='numpy|Pillow|scipy|astropy'
+    PIP_HAVE=`python -m pip list | egrep "$PIP_NEED"`
+    if [ `echo "$PIP_HAVE" | wc -l` -ne 4 ]; then
+        echo "We need all of the following Python packages: `echo "$PIP_NEED" | sed 's/|/ /g'`"
+        echo But you only have the following:
+        echo "$PIP_HAVE"
+        echo "Please use 'python2.7 -m pip install <package>' to install any missing packages"
+        die "missing pip packages"
+    fi
+else
+    python2.7 --version || die "You need to install Python 2.7, and have the executable called python2.7"
+    PIP_NEED='numpy|Pillow|scipy|astropy'
+    PIP_HAVE=`python2.7 -m pip list | egrep "$PIP_NEED"`
+    if [ `echo "$PIP_HAVE" | wc -l` -ne 4 ]; then
+        echo "We need all of the following Python packages: `echo "$PIP_NEED" | sed 's/|/ /g'`"
+        echo But you only have the following:
+        echo "$PIP_HAVE"
+        echo "Please use 'python2.7 -m pip install <package>' to install any missing packages"
+        die "missing pip packages"
+    fi
 fi
 
-MYDIR=`dirname $0`
-MYDIR=`cd $MYDIR; /bin/pwd`
+# Source of following line: https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 case $# in
 0) export SPARCFIRE_HOME="$MYDIR" ;;
 *) export SPARCFIRE_HOME="$1";;
