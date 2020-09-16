1e6e0ca8fa30c7da826102b1ef62396f3e1ce2fa
JohnEEnglish <jedwardenglish@gmail.com>
2020-09-15 21:27:24 -0700

    Anthony updated the Ubuntu Install Script.

--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -1,47 +1,44 @@
 #!/bin/sh
 echo "\n\n---Gettting latest package lists---\n\n"
-sudo apt-get update
+apt-get update
+apt-get install unzip
 
 echo "\n\n---Installing GIT---\n\n"
-sudo apt-get install git -y
+apt-get install git -y
 
 echo "\n\n---Installing GCC---\n\n"
-sudo apt install gcc -y
+apt install gcc -y
 
 
 echo "\n\n---Installing Python2.7, numpy, astropy, scipy, and Pillow---\n\n"
-sudo apt install python2.7 python-pip -y
+apt install python2.7 python-pip -y
 pip2 install numpy
 pip2 install astropy
 pip2 install scipy
 pip2 install Pillow
 
 # create a temporary directory to download and unzip matlab installs
-sudo mkdir /matlab_downloads/
+mkdir /matlab_downloads/
 
 echo "\n\n---Downloading MATLAB Compiler 2017a---\n\n"
-sudo wget -P /matlab_downloads/ https://ssd.mathworks.com/supportfiles/downloads/R2017a/deployment_files/R2017a/installers/glnxa64/MCR_R2017a_glnxa64_installer.zip
-sudo unzip /matlab_downloads/MCR_R2017a_glnxa64_installer.zip -d /matlab_downloads/
-echo "\n\nAbout to install MATLAB Compiler 2017a."
-echo "IMPORTANT: When prompted set the installation folder to /pkg/matlab"
-echo "	This must be done correctly for SpArcFiRe to work."
-echo "Press ENTER when ready to install."
-read -p "" key
-sudo /matlab_downloads/install
+wget -P /matlab_downloads/ https://ssd.mathworks.com/supportfiles/downloads/R2017a/deployment_files/R2017a/installers/glnxa64/MCR_R2017a_glnxa64_installer.zip
+unzip /matlab_downloads/MCR_R2017a_glnxa64_installer.zip -d /matlab_downloads/
+echo "\n\nInstalling MATLAB Compiler 2017a."
+/matlab_downloads/install -mode silent -agreeToLicense yes -destinationFolder /pkg/matlab
 
 # cleanup all temp directories
-sudo rm -r /matlab_downloads/*
-sudo rmdir /matlab_downloads
+rm -r /matlab_downloads/*
+rmdir /matlab_downloads
 
 # rename directory to the one SpArcFiRe expects
-sudo mv /pkg/matlab/v92 /pkg/matlab/R2017a
+mv /pkg/matlab/v92 /pkg/matlab/R2017a
 
 echo "\n\n---Cloning SpArcFiRe---\n\n"
-git clone https://github.com/wschallo/SpArcFiRe.git
+sudo -u username git clone https://github.com/wschallo/SpArcFiRe.git
 
 
 echo "\n\n---Setting Up SpArcFiRe---\n\n"
-SpArcFiRe/scripts/wschallo/setup.sh
+sudo -u username SpArcFiRe/scripts/wschallo/setup.sh
 
 echo "Setup Complete, to run SpArcFiRe on the test images run ~/bin/wschallo/SpArcFiRe-run.sh"
-echo "For a more in-depth tutorial on how to use SpArcFiRe, visit the github page under intructions."
+echo "For an in-depth tutorial on how to use SpArcFiRe read the README file or look at it on the GitHub repo."
