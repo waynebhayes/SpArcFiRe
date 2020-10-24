ec43d69c810ae891bed0d5347ae34576b526d191
Wayne Hayes <whayes@uci.edu>
2020-10-24 12:55:07 -0700

    simplifying the logic of setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ die() { echo "FATAL ERROR: $@" >&2; return 1
 }
 USAGE="USAGE:
 
-    source $0 [SpArcFiRe Repo Directory]
+    source ./setup.sh [SpArcFiRe Repo Directory]
 
 This script does not change any files, but sets the environment variable SPARCFIRE_HOME to
 the base of the SpArcFiRe repo---which is usually just the same directory this
@@ -13,38 +13,35 @@ directory.
 Note: the 'source' command assumes you're using bash as your shell; if not, you'll need
 to figure out how to set the environment variable SPARCFIRE_HOME yourself.
 "
+NL='
+' # newline as a variable, for ease later
 
 echo -n "Checking you have Python 2.7 installed"
-PY_VERSION=`python --version`;
-if [[ $PY_VERSION == "2.7"* ]]; then
-    PIP_NEED='numpy|Pillow|scipy|astropy'
-    PIP_HAVE=`python -m pip list | egrep "$PIP_NEED"`
-    if [ `echo "$PIP_HAVE" | wc -l` -ne 4 ]; then
-	echo "$USAGE"
-        echo "We need all of the following Python packages: `echo "$PIP_NEED" | sed 's/|/ /g'`"
-        echo But you only have the following:
-        echo "$PIP_HAVE"
-        echo "Please use 'python2.7 -m pip install <package>' to install any missing packages"
-        echo ""
-        echo SpArcFiRe repo is in "$SPARCFIRE_HOME"
-        echo "If you ran this script without the word 'source' before it, you messed up. Try again."
-	die "missing pip packages"
-    fi
+if python --version | fgrep 2.7 2>/dev/null; then
+    PYTHON=python
+elif python2.7 --version 2>/dev/null; then
+    PYTHON=python2.7
 else
-    python2.7 --version || die "You need to install Python 2.7, and have the executable called python2.7"
-    PIP_NEED='numpy|Pillow|scipy|astropy'
-    PIP_HAVE=`python2.7 -m pip list | egrep "$PIP_NEED"`
-    if [ `echo "$PIP_HAVE" | wc -l` -ne 4 ]; then
-        echo "$USAGE"
-	echo "We need all of the following Python packages: `echo "$PIP_NEED" | sed 's/|/ /g'`"
-        echo But you only have the following:
-        echo "$PIP_HAVE"
-        echo "Please use 'python2.7 -m pip install <package>' to install any missing packages"
-        echo ""
-        echo SpArcFiRe repo is in "$SPARCFIRE_HOME"
-        echo "If you ran this script without the word 'source' before it, you messed up. Try again."
-	die "missing pip packages"
-    fi
+    die "You need to install Python 2.7, and have the executable called python2.7"
+fi
+echo "Your python2.7 is called $PYTHON:"
+$PYTHON --version
+
+PIP_NEED='numpy|Pillow|scipy|astropy'
+PIP_HAVE=`$PYTHON -m pip list | egrep "$PIP_NEED"`
+if [ `echo "$PIP_HAVE" | wc -l` -ne 4 ]; then
+    echo "$USAGE"
+    echo "We need all of the following Python packages: `echo "$PIP_NEED" | sed 's/|/ /g'`"
+    echo But you only have the following:
+    echo "$PIP_HAVE"
+    echo "To get the missing packages, please execute following commands:"
+    echo ""
+    echo "$PIP_NEED" | tr '|' "$NL" | fgrep -v -f <(echo "$PIP_HAVE" | tr ' ' "$NL") |
+	awk '{printf "\t'$PYTHON' -m pip install %s\n",$0}'
+    echo ""
+    echo SpArcFiRe repo is in "$SPARCFIRE_HOME"
+    echo "If you ran this script without the word 'source' before it, you messed up. Try again."
+    die "missing pip packages"
 fi
 
 # Source of following line: https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
