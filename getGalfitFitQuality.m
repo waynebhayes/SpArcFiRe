0195f5557c2a48726960cb9b789c1c18686c9bd6
Matthew Portman <29168957+MatthewPortman@users.noreply.github.com>
2021-01-15 18:33:43 -0800

    Updating...
    Updated getGalfitFitQuality.m to cd into a tmp directory before running GALFIT so that it dumps all the unnecessary files in there. Furthermore, now only one GALFIT file will be retained in the final directory (at my own behest) just in case.
    
    I also updated the position of cropRad in writeGxyParamsToCsv.m so it's easier to find later and once we update the reg tests, everything should be good to go.

---
+++ getGalfitFitQuality.m
@@ -0,0 +1,131 @@
+function [result] = getGalfitFitQuality(img,clusReproj,outputPath,gxyParams)
+% Performs and return a GALFIT model fitting on the input image.
+% INPUTS:
+%   img: the image to use for preprocessing
+%   outputPath: the path to write and store the GALFIT .feedme/.fits input
+%       and .fits output files
+%   gxyParams: galaxy parameters used for generating the GALFIT initial
+%       fit .feedme input file.
+% OUTPUTS:
+%   img: the preprocessed image
+%   imgNoUsm: the preprocessed image, without applying the unsharp mask
+%   gxyParams: structure containing some information about the ellipse fit 
+
+    % Big edits coming in - Matthew 1/12/21
+    % Fixing where GALFIT runs and retaining only the final FITS
+    % for posterity and potential analysis/reporduction
+
+    % Changing outputPath to drop everything into tmp
+    
+    OGoutputPath = outputPath;
+    gal_name = split(string(outputPath), '/');
+    gal_name = gal_name(end);
+    outputPath = '/tmp/galfit_junk/' + gal_name;
+
+    %disp('Checking outputPath - ' + outputPath)
+    outputPath = char(outputPath);
+    fitswrite(img, [outputPath '_galfit_input.fits']);
+    %galfitTemplateFilename = ['/home/' getenv('USER') '/bin/GalfitTemplates/template.feedme']; %added this to call galfit using correct path
+    galfitTemplateFilename = [getenv('SPARCFIRE_HOME') '/scripts/GalfitTemplates/template.feedme'];
+    disp(['Reading GALFIT template file: ' galfitTemplateFilename])
+    galfitTemplate = fopen(galfitTemplateFilename,'r');
+    text = fread(galfitTemplate, '*char')';
+    fclose(galfitTemplate);
+
+    % Fill out template by replacing variables with their actual values
+    muFit = gxyParams.iptCtrXY;
+    nRows = gxyParams.iptSz(1);
+    nCols = gxyParams.iptSz(2);
+    text = strrep(text, '$input_name', [outputPath '_galfit_input.fits']);
+    text = strrep(text, '$output_name', [OGoutputPath '_galfit_output.fits']);
+    text = strrep(text, '$x_center', num2str(muFit(1)));
+    text = strrep(text, '$y_center', num2str(size(img, 1) - muFit(2) + 1));
+    text = strrep(text, '$radius_bulge', num2str(gxyParams.bulgeMajAxsLen / 4));
+    text = strrep(text, '$radius_disk', num2str(gxyParams.diskMajAxsLen / 4));
+    text = strrep(text, '$sersic_index', num2str(1.0));
+    text = strrep(text, '$axis_ratio', num2str(gxyParams.diskAxisRatio));
+    text = strrep(text, '$position_angle', num2str(90 - rad2deg(gxyParams.diskMajAxsAngleRadians)));
+    text = strrep(text, '$x_max', num2str(nRows));
+    text = strrep(text, '$y_max', num2str(nCols));
+
+    galfitInput = fopen([outputPath '.feedme'],'wt');
+    fwrite(galfitInput, text);
+    fclose(galfitInput);
+
+    % Run galfit
+    %galfitCommand = ['/home/' getenv('USER') '/bin/galfit ' outputPath '.feedme'];
+    % ^Will 9/30/19: This line of code was added a few months ago because of how matlab evaluates path's at compile time
+    % see documentation/stuck_try_this.txt for more explanation
+    galfitCommand = [getenv('SPARCFIRE_HOME') '/scripts/galfit ' outputPath '.feedme'];
+
+    % Grabbing current directory before cd-ing to tmp
+    former_dir = cd('/tmp/galfit_junk');
+
+    system(galfitCommand);
+
+    %disp(former_dir)
+    %disp(class(former_dir))
+    cd(former_dir);
+
+    % Retrieve input/model subtraction
+    model = mat2gray(fitsread([OGoutputPath '_galfit_output.fits'], 'image', 2));
+    residual = mat2gray(fitsread([OGoutputPath '_galfit_output.fits'], 'image', 3));
+    binarizedClusters = imbinarize(rgb2gray(clusReproj), 0.1);
+    binarizedModel = imbinarize(model);
+    binarizedResidual = imbinarize(residual);
+    relevantElements = binarizedModel.*binarizedResidual;
+    
+   % disp(mean2(model));
+   % disp(mean2(residual));
+   % disp(mean2(binarizedResidual));
+   % residualMedian = medfilt2(residual, 'symmetric');
+   % residual2Median = medfilt2(binarizedResidual, 'symmetric');
+   % figure(1);
+   % imshow(residual);
+   % figure(2);
+   % imshow(residualMedian);
+   % figure(3);
+   % imshow(residual2Median);
+   %  figure(3);
+   %  imshow(binarizedResidual);
+   %  figure(4);
+   %  imshow(relevantElements);
+    
+    % Calculate number of true positives.
+    selectedElements = binarizedClusters;
+    %relevantElements = binarizedModel.*binarizedResidual;
+    truePositiveElements = times(relevantElements, selectedElements);
+    truePositives = sum(sum(times(relevantElements, selectedElements)));
+    disp(['     truePositives: ' int2str(truePositives)])
+
+    % Calculate F1 confidence.
+    precision = truePositives / sum(sum(selectedElements));
+    recall = truePositives / sum(sum(relevantElements));
+    disp(['     precision: ' num2str(precision)]);
+    disp(['     recall: ' num2str(recall)]);
+    f1confidence = (2 * precision * recall) / (precision + recall);
+    disp(['     f1confidence: ' num2str(f1confidence)]);
+    gxyParams.fitQualityF1 = f1confidence;
+
+    pearsonCorrelationCoefficient = corr2(residual, selectedElements);
+    gxyParams.fitQualityPCC = pearsonCorrelationCoefficient;
+    result = gxyParams;
+
+    % Writing images
+    grouped =  cat(2,model,residual,relevantElements,selectedElements,truePositiveElements);
+    %imwrite(model, [outputPath '-L1_model.png']);
+    %imwrite(residual, [outputPath '-L2_residual.png']);
+    %imwrite(relevantElements, [outputPath '-L3_maskedResidual.png']);
+    %imwrite(selectedElements, [outputPath '-L4_clusMask.png']);
+    %imwrite(truePositiveElements, [outputPath '-L5_maskedClustersResidual.png']);
+    %imwrite(grouped, [outputPath '-L_fitQuality.png']);
+
+    % Cleanup files - let's save it for the end shall we? Matthew 1/12/21
+    %disp('Removing galfit.* files...')
+    %delete([outputPath 'galfit.*']);
+    %delete(['galfit.*']); 
+    %delete([outputPath '.feedme']);
+    %delete([outputPath '_galfit_input.fits']);
+    %delete([outputPath '_galfit_output.fits']);
+end
+
---
+++ writeGxyParamsToCsv.m
@@ -0,0 +1,248 @@
+function writeGxyParamsToCsv(resPath, imgSetName, outDir, aggregate_matfiles)
+
+if nargin < 3 || isempty(outDir)
+    outDir = resPath;
+end
+
+if nargin < 4 || isempty(aggregate_matfiles)
+    aggregate_matfiles = false;
+end
+
+if ~isdir(resPath)
+    error('%s is not a directory\n', resPath);
+end
+
+if outDir(end) ~= filesep
+    outDir(end+1) = filesep;
+end
+if resPath(end) ~= filesep
+    resPath(end+1) = filesep;
+end
+
+% fileNames = ls(resPath);
+% fileNames = strtrim(mat2cell(fileNames, ...
+%     ones(1, size(fileNames, 1)), size(fileNames, 2)));
+fileNames = dir(resPath);
+fileNames = {fileNames.name};
+
+fileNames = fileNames(~cellfun(@isempty, ...
+    strfind(fileNames, 'gxyParamsList-part-')))
+
+next_file_idx = 1;
+sampleGxyParams = struct();
+while(~isfield(sampleGxyParams, 'length_thresholds') && (next_file_idx <= length(fileNames)))
+    curParamsList = load([resPath fileNames{next_file_idx}]);
+    curParamsList = curParamsList.gxyParamsList;
+    next_gxy_idx = 1;
+    while(~isfield(sampleGxyParams, 'length_thresholds') && (next_gxy_idx <= length(curParamsList)))
+        sampleGxyParams = curParamsList{next_gxy_idx};
+        next_gxy_idx = next_gxy_idx + 1;
+    end
+    next_file_idx = next_file_idx + 1;
+end
+if next_file_idx == 1
+    error('no gxyParamsList files found')
+end
+if ~isfield(sampleGxyParams, 'length_thresholds')
+    error('None of the given galaxies have any detected arcs')
+end
+
+gxyParamsNames = {'badBulgeFitFlag', 'bulgeAxisRatio', 'bulgeMajAxsLen',...
+    'bulgeMajAxsAngle', 'bulgeAvgBrt', 'bulgeDiskBrtRatio', 'numElpsRefits',...
+    'diskAxisRatio', 'diskMinAxsLen', 'diskMajAxsLen', 'diskMajAxsAngleRadians',...
+    'inputCenterR', 'inputCenterC', 'iptSz', 'muDist', 'muDistProp', 'covarFit', ...
+    'cropRad', 'wtdLik', 'likOfCtr',... # Added cropRad here 1/7/21
+    'brtUnifScore', 'gaussLogLik', 'contourBrtRatio', 'standardizedCenterR',...
+    'standardizedCenterC', 'hasDeletedCtrClus', ...
+    'failed2revDuringMergeCheck', 'failed2revDuringSecondaryMerging',...
+    'failed2revInOutput', ...
+    'fitQualityF1','fitQualityPCC',...
+    'cpu_time', 'wall_time',...
+    'bar_candidate_available', 'bar_used',...
+    'alenAt25pct', 'alenAt50pct', 'alenAt75pct', 'rankAt25pct', ...
+    'rankAt50pct', 'rankAt75pct', 'avgArcLength', 'minArcLength', ...
+    'lowerQuartileArcLength', 'medianArcLength', 'upperQuartileArcLength',... 
+    'maxArcLength', 'totalArcLength', 'totalNumArcs', 'chirality_votes_maj',...
+    'chirality_votes_alenWtd', 'alenWtdPangSum', 'top2_chirality_agreement',...
+    'pa_longest', 'pa_avg', 'pa_avg_abs', 'pa_avg_domChiralityOnly', ...
+    'pa_alenWtd_avg', 'paErr_alenWtd_stdev', ...
+    'pa_alenWtd_avg_abs', 'paErr_alenWtd_stdev_abs', ...
+    'pa_alenWtd_avg_domChiralityOnly', 'paErr_alenWtd_stdev_domChiralityOnly',...
+    'pa_totBrtWtd', 'pa_avgBrtWtd', 'pa_alenWtd_median', ...
+    'pa_alenWtd_lowQuartile', 'pa_alenWtd_highQuartile', ...
+    'pa_alenWtd_lowDecile', 'pa_alenWtd_highDecile', ...
+    'pa_alenWtd_median_domChiralityOnly', 'pa_alenWtd_lowQuartile_domChiralityOnly',...
+    'pa_alenWtd_highQuartile_domChiralityOnly', 'pa_alenWtd_lowDecile_domChiralityOnly',...
+    'pa_alenWtd_highDecile_domChiralityOnly', 'sorted_agreeing_pangs', ...
+    'sorted_agreeing_arclengths', ...
+    'numArcs_largest_length_gap', 'numDcoArcs_largest_length_gap',...
+    'numArcs_arclength_function_flattening', 'numDcoArcs_arclength_function_flattening'};
+    % Added cropRad - Matthew P. 1/7/21
+gxyParamsNamesExcluded = setdiff(fields(sampleGxyParams), gxyParamsNames);
+for ii=1:length(gxyParamsNamesExcluded)
+    %disp('Exclusions from gxyParams:\n');
+    fprintf('%s\n', gxyParamsNamesExcluded{ii});
+end
+fields(sampleGxyParams)
+armCountThresholds = sampleGxyParams.length_thresholds;
+
+csvFile = fopen([outDir imgSetName '.csv'], 'wt');
+
+fprintf(csvFile, 'name, fit_state, warnings, star_mask_used, noise_mask_used, chirality_maj, chirality_alenWtd, chirality_wtdPangSum, chirality_longestArc, ');
+fprintf(csvFile, '%s, ', gxyParamsNames{:});
+fprintf(csvFile, ['bar_score_img, bar_cand_score_orifld, bar_angle_input_img, bar_half_length_input_img, '...
+    'bar_angle_standardized_img, bar_half_length_standardized_img, ']);
+for thresIdx=1:1:length(armCountThresholds)
+%     nxt_header_item = strrep(...
+%         sprintf('numArcsGE%.1f, ', armCountThresholds(thresIdx)), '.', 'pt');
+%     fprintf(csvFile, nxt_header_item);
+    fprintf(csvFile, 'numArcsGE%03d, ', round(armCountThresholds(thresIdx)));
+end
+for thresIdx=1:1:length(armCountThresholds)
+%     nxt_header_item = strrep(...
+%         sprintf('numDcoArcsGE%.1f, ', armCountThresholds(thresIdx)), '.', 'pt');
+%     fprintf(csvFile, nxt_header_item);
+    fprintf(csvFile, 'numDcoArcsGE%03d, ', round(armCountThresholds(thresIdx)));
+end
+fprintf(csvFile, '\n');
+
+arcCsvFile = fopen([outDir imgSetName '_arcs.csv'], 'wt');
+
+fprintf(arcCsvFile, ['gxyName,alenRank,math_initial_theta,pitch_angle,math_initial_radius,'...
+    'relative_theta_start,relative_theta_end,r_start,r_end,used2rev,failed2rev,hasUndefinedEndpoints,'...
+    'arc_length,num_pixels,err_per_len,err_per_pixel,'...
+    'mean_intensity,brt_ratio_score,anovaF_score,clusOutputColor\n']);
+
+% for ii=1:1:length(fileNames)
+%     paramsListPart = load([resPath fileNames{ii}], 'gxyParamsList');
+%     paramsListPart = paramsListPart.gxyParamsList;
+%     paramsListPart = paramsListPart(cellfun(@(x)(~isempty(x)), paramsListPart));
+%     gxyParamsList = [gxyParamsList; paramsListPart];
+% end
+
+if aggregate_matfiles
+    gxyParamsList = {};
+end
+
+imgNum = 0;
+for fIdx=1:1:length(fileNames)
+    curGxyParamsList = load([resPath fileNames{fIdx}], 'gxyParamsList');
+    curGxyParamsList = curGxyParamsList.gxyParamsList;
+    curGxyParamsList = curGxyParamsList(cellfun(@(x)(~isempty(x)), curGxyParamsList));
+    for imgIdx = 1:1:length(curGxyParamsList)
+        imgNum = imgNum + 1;
+        if mod(imgNum, 100) == 0
+            fprintf('processing image %d\n', imgNum);
+        end
+        curParams = curGxyParamsList{imgIdx};
+        writeCsvEntry(curParams, csvFile, gxyParamsNames);
+        writeArcsCsvEntry(curParams, arcCsvFile)
+    end
+    if aggregate_matfiles
+        gxyParamsList = [gxyParamsList; paramsListPart];
+    end
+end
+
+fclose(csvFile);
+fclose(arcCsvFile);
+
+fprintf('done writing CSV files.\n');
+
+if aggregate_matfiles
+    save([resPath 'gxyParamsList-combined'], 'gxyParamsList');
+end
+
+end
+
+function writeCsvEntry(curParams, csvFile, gxyParamsNames)
+    fprintf(csvFile, [curParams.name ', ']);
+    
+    if strmatch('OK', curParams.fit_state)
+        fprintf(csvFile, 'OK, ');
+    else
+%         fprintf(csvFile, [curParams.fit_state ', ' repmat(', ', 1, ...
+%             (length(fields(curParams)) + length(armCountThresholds) - 3)) '\n']);
+        fprintf(csvFile, [strrep(strrep(curParams.fit_state, sprintf('\n'), ' '), ',', ' ') '\n']);
+        return;
+    end
+    
+    curWarnings = curParams.warnings;
+    if isempty(curWarnings)
+        fprintf(csvFile, '(no warnings),');
+    else
+        for ii=1:1:length(curWarnings)
+            fprintf(csvFile, [curWarnings{ii} '|']);
+        end
+        fprintf(csvFile, ',');
+    end
+    
+    fprintf(csvFile, '%s ,', curParams.starMaskUsed);
+    fprintf(csvFile, '%s ,', curParams.noiseMaskUsed);
+    
+    chVotes = curParams.chirality_votes_maj;
+    if chVotes(1) > chVotes(2)
+        fprintf(csvFile, 'S-wise, ');
+    elseif chVotes(1) < chVotes(2)
+        fprintf(csvFile, 'Z-wise, ');
+    else
+        fprintf(csvFile, 'EQ, ');
+    end
+    chVotes = curParams.chirality_votes_alenWtd;
+    if chVotes(1) > chVotes(2)
+        fprintf(csvFile, 'S-wise, ');
+    elseif chVotes(1) < chVotes(2)
+        fprintf(csvFile, 'Z-wise, ');
+    else
+        fprintf(csvFile, 'EQ, ');
+    end
+    if curParams.alenWtdPangSum > 0
+        fprintf(csvFile, 'S-wise, ');
+    elseif curParams.alenWtdPangSum < 0
+        fprintf(csvFile, 'Z-wise, ');
+    else
+        fprintf(csvFile, 'EQ, ');
+    end
+    if curParams.chirality_longest_arc > 0
+        fprintf(csvFile, 'S-wise, ');
+    elseif curParams.chirality_longest_arc < 0
+        fprintf(csvFile, 'Z-wise, ');
+    else
+        fprintf(csvFile, 'EQ, ');
+    end
+    for fldIdx = 1:1:length(gxyParamsNames)
+        fprintf(csvFile, '%s, ', mat2str(getfield(curParams, gxyParamsNames{fldIdx}), 10));
+    end
+    barInfo = curParams.bar_info;
+    fprintf(csvFile, '%2.4f, ', barInfo.score);
+    fprintf(csvFile, '%2.4f, ', barInfo.candScore);
+%     if barInfo.barDetected
+        fprintf(csvFile, '%2.4f, %2.4f, %2.4f, %2.4f, ', ...
+            barInfo.iptAngle * (180/pi), barInfo.iptHalfLength,...
+            barInfo.stdzAngle * (180/pi), barInfo.stdzHalfLength);
+%     else
+%         fprintf(csvFile, 'noBarDetected, noBarDetected, ');
+%     end
+   
+    armCounts = curParams.thres_arm_counts;
+%     armCounts = armCounts(:, 2);
+    fprintf(csvFile, '%d, ', armCounts);
+    armCountsDco = curParams.thres_arm_counts_dco;
+    fprintf(csvFile, '%d, ', armCountsDco);
+    fprintf(csvFile, '\n');
+end
+
+function writeArcsCsvEntry(curGxyParams, arcCsvFile)
+    curName = curGxyParams.name;
+    if ~isfield(curGxyParams, 'arc_params') % image rejection or preprocessing failure
+        return;
+    end
+    pitchAngleIndex = 2;
+    curArcParams = curGxyParams.arc_params;
+    curArcParams(:, pitchAngleIndex) = ...
+        atan(curArcParams(:, pitchAngleIndex) * (pi/180)) * (180/pi);
+    curClusColors = curGxyParams.arcClusterColors;
+    for arcIdx=1:1:size(curArcParams, 1)
+        fprintf(arcCsvFile, '%s,%d,%f,%f,%f,%f,%f,%f,%f,%d,%d,%d,%f,%f,%f,%f,%f,%f,%f,%s\n',...
+            curName, arcIdx, curArcParams(arcIdx, :), mat2str(curClusColors(arcIdx, :), 4));
+    end
+end
