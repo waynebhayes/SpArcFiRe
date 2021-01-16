a939e7a5499e57b5e2357f8a9af882f910cb8d0e
Matthew Portman <29168957+MatthewPortman@users.noreply.github.com>
2021-01-15 18:34:49 -0800

    Updating...
    Now Pedro's script doesn't do anything fancy but delete the tmp/galfit_junk directory and remake it. Moving GALFIT to tmp solved the problem he was tackling with this script, so - sorry Pedro!

--- scripts/moveGalfitFiles.sh
+++ scripts/moveGalfitFiles.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
+# Matt - 1/15/21
+# Changing the script's utility to instead delete all galfit junk
+# Ignore below description
+
 #This is meant to clean up galfit files created when using the generateFitQuality option.
 #Galfit will output a series of files, called galfit.01, galfit.02, ... etc
 #This script simply moves the file to the corresponding galaxy directory
 
-#TODO check if the generateFitQuality variable has been set so that this is safer
+# TODO: Check if the generateFitQuality variable has been set so that this is safer
 
 INDIR=$1 #original directory that the script was called from
 BASEOUTDIR=$2 #directory where all of SpArcFiRe's output directories are
+gjunk="/tmp/galfit_junk"
 
-echo "Moving Galfit Files"
+#echo "Moving Galfit Files"
+echo "Deleting GALFIT Files from $gjunk..."
 
-for file in $(find "$INDIR" -iregex '.*/galfit\.[0-9]+')
-do
-    echo "moving file $file"
-    secondLine=$(sed '2q;d' $file)
-    out=$(echo "$secondLine" | cut -c 1-20 --complement )
-    outdir=$(echo "$out" | rev | cut -d '/' -f2- | rev)
-    if [[ -d "$outdir" ]]
-    then
-        outpath=$(echo "$out" | rev | cut -c 1-7 --complement | rev)
-        outpath="$outpath-galfit_control_params"
-        mv $file $outpath
-    fi
-done
+rm -rf $gjunk
+mkdir $gjunk
 
-mv "$INDIR/fit.log" "$BASEOUTDIR/galfit_fit.log"
+#for file in $(find "$INDIR" -iregex '.*/galfit\.[0-9]+')
+#do
+#    echo "moving file $file"
+#    secondLine=$(sed '2q;d' $file)
+#    out=$(echo "$secondLine" | cut -c 1-20 --complement )
+#    outdir=$(echo "$out" | rev | cut -d '/' -f2- | rev)
+#    if [[ -d "$outdir" ]]
+#    then
+#        outpath=$(echo "$out" | rev | cut -c 1-7 --complement | rev)
+#        outpath="$outpath-galfit_control_params"
+#        mv $file $outpath
+#    fi
+#done
+
+#mv "$INDIR/fit.log" "$BASEOUTDIR/galfit_fit.log"
