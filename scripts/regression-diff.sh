88bad90c94e3c11edc1fc74b7bd24ac6573d08d0
Wayne Hayes <whayes@uci.edu>
2021-06-02 16:50:09 -0700

    regression-diff.sh now knows about variable names in headers

--- scripts/regression-diff.sh
+++ scripts/regression-diff.sh
@@ -1,28 +1,68 @@
 #!/bin/sh
 DIR="`dirname $0`"
 awk "`cat $DIR/misc.awk`"'
-    BEGIN{MAX_REL_ERR=1e-4}
+    BEGIN{MAX_REL_ERR=0.01}
+    FNR==1{
+	F[ARGIND]=FILENAME;
+	if(index(FILENAME,".tsv")){
+	    ++numTSVs;
+	    for(i=1;i<=NF;i++)varCols[$i][ARGIND]=i; # record column number of each variable name
+	}
+    }
     {for(i=1;i<=NF;i++)L[ARGIND][FNR][i]=$i}
+    function CheckCol(l, varName, col1, col2) {
+	if(!index(varName,"time") && L[1][l][col1]!=L[2][l][col2]) {
+	    printErrMsg = 0;
+	    isNum = 1*L[1][l][col1] + 1*L[2][l][col2];
+	    if(isNum) {
+		if(L[1][l][col1]==int(L[1][l][col1]) && L[2][l][col2]!=int(L[2][l][col2])) printErrMsg=1 # both are integers
+		else if(1*L[2][l][col2]) { # correct one is non-zero, so we can compute a relative error
+		    rel_err = (L[1][l][col1] - L[2][l][col1])/L[2][l][col2];
+		    if(ABS(rel_err) > MAX_REL_ERR) printErrMsg=1
+		}
+		else printErrMsg=1 # correct one is zero but new one is not
+	    }
+	    if(printErrMsg) {
+		++diff;
+		Warn(sprintf("line %d columns (%d,%d) (%s,%s)\n\t<%s\n\t>%s\n",l,col1,col2, L[1][1][col1], L[2][1][col2], L[1][l][col1], L[2][l][col2]))
+	    }
+	}
+    }
     END{
 	diff=0;
 	ASSERT(ARGIND==2, "expecting exactly two filenames as input");
-	a=ARGIND;
-	for(l=1;l<=length(L[a]);l++) for(i=1;i<=length(L[a][l]);i++) {
-	    colName=L[1][1][i];
-	    if(!index(colName,"time") && L[1][l][i]!=L[a][l][i]) {
-		printErrMsg = 0;
-		isNum = 1*L[1][l][i] + 1*L[a][l][i];
-		if(isNum) {
-		    if(L[1][l][i]==int(L[1][l][i]) && L[a][l][i]!=int(L[a][l][i])) printErrMsg=1 # both are integers
-		    else if(1*L[a][l][i]) { # correct one is non-zero, so we can compute a relative error
-			rel_err = (L[1][l][i] - L[a][l][i])/L[a][l][i];
-			if(ABS(rel_err) > MAX_REL_ERR) printErrMsg=1
-		    }
-		    else printErrMsg=1 # correct one is zero but new one is not
+	# Check that both files have the same number of columns, especially on the header line
+	if(length(L[1]) != length(L[2])){++diff; Warn(sprintf("\"%s\" has %d lines but \"%s\" has %d",
+	    F[1], length(L[1]), F[2], length(L[2])))}
+
+	if(isarray(varCols)) {
+	    ASSERT(numTSVs==ARGIND, "all files must be TSVs, or none");
+	    headerMismatch=0;
+	    PROCINFO["sorted_in"]="@ind_num_asc"; #traverse for loop based on integer VALUE (not INDEX) of elements
+	    for(i in varCols) if(length(varCols[i])!=numTSVs){
+		Warn(sprintf("\"%s\" is not a header in all input files",i));
+		delete varCols[i];
+		++headerMismatch;
+	    }
+	    if(length(L[1][1]) != length(L[2][1])) {++diff; Warn(sprintf("\"%s\" has %d columns but \"%s\" has %d",
+		F[1], length(L[1][1]), F[2], length(L[2][1])))}
+	    else if(!headerMismatch) for(i=0;i<=1;i++) for(j in L[i+1][1]){
+		other=1-i;
+		if(L[i+1][1][j] != L[other+1][1][j]) {
+		    ++diff;
+		    Warn(sprintf("column %d of \"%s\" is named \"%s\", but the other file has name \"%s\" there",
+			j,F[i+1],L[i+1][1][j], L[other+1][1][j]))
 		}
-		if(printErrMsg) {diff=1; printf "line %d column %d (%s)\n<%s\n>%s\n",l,i, L[1][1][i], L[1][l][i], L[2][l][i] >"/dev/stderr"}
 	    }
 	}
+	else
+	    ASSERT(numTSVs==0, "all files must be TSVs, or none");
+
+	for(l=2;l<=length(L[1]);l++)
+	    if(numTSVs)
+		for(v in varCols) CheckCol(l, v, varCols[v][1], varCols[v][2])
+	    else
+		for(i=1;i<=length(L[1][l]);i++) CheckCol(l, "", i, i)
 	exit(diff);
     }' "$@"
 exit $?
