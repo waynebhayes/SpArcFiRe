7ae6dd393624872a0a239c5e94d7de202c365ac1
Matthew Portman <29168957+MatthewPortman@users.noreply.github.com>
2022-10-21 09:14:19 -0700

    Update setup.bash
    To fix setup bugs and things in order to merge in the future

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-USAGE="USAGE:
+SETUP_USAGE="USAGE:
 
-    source ./setup.sh [SpArcFiRe Repo Directory]
+    source setup.bash [SpArcFiRe Repo Directory]
 
 This script does not change any files, but sets the environment variable SPARCFIRE_HOME to
 the base of the SpArcFiRe repo---which is usually just the same directory this
@@ -11,13 +11,14 @@ directory.
 Note: the 'source' command assumes you're using bash as your shell; if not, you'll need
 to figure out how to set the environment variable SPARCFIRE_HOME yourself.
 "
-die() { (echo "$USAGE"; echo "FATAL ERROR: $@") >&2; return 1
+fail() { echo "$@" >&2; return 1
 }
 
 [ "$SPARCFIRE_HOME" != "" ] && return
 
 if [ "`echo $0 | sed 's,.*/,,'`" = setup.sh ]; then
-    die "You've run this script; source it instead by typing:
+    fail "$SETUP_USAGE 
+    SETUP ERROR: You've run this script; source it instead by typing:
     source $0"
 fi
 
@@ -32,37 +33,32 @@ case $# in
 esac
 if [ "$SPARCFIRE_HOME" = . ]; then SPARCFIRE_HOME=`/bin/pwd`; fi
 
-#echo -n "Checking you have Python 2.7 installed"
 echo -n "Checking you have Python 3 installed"
 if python --version | fgrep 3 2>/dev/null; then
     PYTHON=python
 elif python3 --version 2>/dev/null; then
     PYTHON=python3
 else
-    #die "You need to install Python 2.7, and have the executable called python2.7"
-    die "You need to install Python 3, and have the executable called python3"
+    fail "$SETUP_USAGE${NL} SETUP ERROR: You need to install Python 3, and have the executable called python3"
 fi
-#echo "Your python2.7 is called $PYTHON:"
 echo "Your python3 is called $PYTHON:"
 $PYTHON --version
 
 PIP_NEED='numpy|Pillow|scipy|astropy'
-#PIP_HAVE=`(pip2 list; $PYTHON -m pip list) 2>/dev/null | awk '{print $1}' | sort -u | egrep "$PIP_NEED"`
 PIP_HAVE=`(pip3 list; $PYTHON -m pip list) 2>/dev/null | awk '{print $1}' | sort -u | egrep "$PIP_NEED"`
 if [ `echo "$PIP_HAVE" | wc -l` -eq 4 ]; then
     echo "SUCCESS! SPARCFIRE_HOME set to $SPARCFIRE_HOME. Now adding $SPARCFIRE_HOME/scripts to PATH."
     export PATH="$SPARCFIRE_HOME/scripts:$PATH"
 else
-    echo "$USAGE"
-    echo "We need all of the following Python packages: `echo "$PIP_NEED" | sed 's/|/ /g'`"
+    (echo "We need all of the following Python packages: `echo "$PIP_NEED" | sed 's/|/ /g'`"
     echo But you only have the following:
-    echo "$PIP_HAVE"
+    if echo $PIP_HAVE | grep . >/dev/null; then echo "$PIP_HAVE"; else echo "    (none)"; fi
     echo "To get the missing packages, please execute following commands, and note you may need to specify '--user':"
     echo ""
     echo "$PIP_NEED" | tr '|' "$NL" | fgrep -v -f <(echo "$PIP_HAVE" | tr ' ' "$NL") |
 	awk '{printf "\t'$PYTHON' -m pip install [--user] %s\n",$0}'
     echo ""
     echo SpArcFiRe repo is in "$SPARCFIRE_HOME"
-    echo "If you ran this script without the word 'source' before it, you messed up. Try again."
-    die "missing pip packages"
+    echo "If you ran this script without the word 'source' before it, you messed up. Try again.") >&2
+    fail "SETUP ERROR: missing pip packages"
 fi
