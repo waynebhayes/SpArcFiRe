74b32b6074e26626250eac6a88a7c1ca0d0a55bc
Matthew Portman <portmanm@uci.edu>
2023-12-29 16:58:39 -0800

    Added option for verticality and argparse to make using the command line easier with this option.

--- GalfitModule/Utilities/generate_png.py
+++ GalfitModule/Utilities/generate_png.py
@@ -1,6 +1,7 @@
 from joblib import Parallel, delayed
 import joblib
 import os
+import argparse
 
 from os.path import join as pj
 from os.path import abspath as absp
@@ -22,25 +23,68 @@ from Classes.Containers import *
 from Classes.FitsHandlers import *
 from Functions.helper_functions import *
 
-def wrapper(gfits, out_png_dir):
+def wrapper(gfits, out_png_dir, vertical = False):
     output = OutputFits(gfits)
-    output.to_png(out_png_dir = out_png_dir)
+    output.to_png(out_png_dir = out_png_dir, vertical = vertical)
 
 
 if __name__ == "__main__":
     
-    out_png_dir = sys.argv[1]
-    list_o_fits = sys.argv[2:]#.split(" ")
+    USAGE = f"""USAGE:
+
+    python3 ./{sys.argv[0]} [OPTION] PNG-OUT-DIRECTORY [FITS-DIRECTORY | SPACE SEPARATED FITS FILES]
+    
+    OPTIONS =>[-vert | --vertical]
+              [-v | --verbose]
+
+    This script is a utility to generate images from output GALFIT FITS files using the to_png 
+    functionality of the OutputFits class.
+    """
+    
+    parser = argparse.ArgumentParser(description = USAGE)
+    
+    parser.add_argument('-vert', '--vertical',
+                        dest     = 'vertical',
+                        action   = 'store_const',
+                        const    = True,
+                        default  = False,
+                        help     = 'Choose to orient the output images from GALFIT vertically as opposed to horizontally.'
+                       )
+    
+    parser.add_argument('-v', '--verbose',
+                        dest     = 'verbose', 
+                        action   = 'store_const',
+                        const    = True,
+                        default  = False,
+                        help     = 'Verbose output. Includes stdout.'
+                       )
+    
+    parser.add_argument(dest     = 'out_png_dir',
+                        help     = "Directory designated to hold the output PNGs."
+                       )
+    
+    parser.add_argument(dest     = 'list_o_fits',
+                        nargs    = "*",
+                        help     = "Either a list of fits files or a directory containing the files to be output to PNG."
+                       )
+    
+    args           = parser.parse_args() # Using vars(args) will call produce the args as a dict
+    out_png_dir    = args.out_png_dir
+    list_o_fits    = args.list_o_fits
+    
+    vertical       = args.vertical
+    verbose        = args.verbose
+    capture_output = not args.verbose
     
     # Assume it's a dir of fits
     if len(list_o_fits) == 1:
-        dir_name = sys.argv[2]
-        list_o_fits = [pj(dir_name, gfit) for gfit in find_files(sys.argv[2], "*.fits")]
+        dir_name = list_o_fits[0]
+        list_o_fits = [pj(dir_name, gfit) for gfit in find_files(dir_name, "*.fits")]
     
     if sp(f"hostname").stdout.split(".")[0] == "bayonet-09":
         if len(list_o_fits) > 500:
-            Parallel(n_jobs = -2)(delayed(wrapper)(gfits, out_png_dir) for gfits in list_o_fits)
+            Parallel(n_jobs = -2)(delayed(wrapper)(gfits, out_png_dir, vertical) for gfits in list_o_fits)
         else:
-            _ = [wrapper(gfits, out_png_dir) for gfits in list_o_fits]
+            _ = [wrapper(gfits, out_png_dir, vertical) for gfits in list_o_fits]
     else:
         print("Not on bayonet, can't generate galaxies.")
